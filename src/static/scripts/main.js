8b8456f6d80491710d1783e395a8f3204dd62824
André Gumieri <andregumieri@gmail.com>
2016-05-21 05:19:39 -0300

    WIP: DOM injection in the restaurant page

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('test after load', jQuery);
+var Templates = {
+	'videoItem': require('../views/video-item.html.js'),
+	'videosWrap': require('../views/videos-wrap.html.js')
+};
+
+jQuery(document).ready(function($) {
+	var $domVideosWrap = $(Mustache.render(Templates['videosWrap'], {'restaurant_name': $('.biz-page-title').text().trim()}));
+	var $container = $domVideosWrap.find('.bbtv-videos-wrap__items-container');
+
+	for(var x=0; x<10; x++) {
+		var $videoItem = $(Mustache.render(Templates['videoItem'], {channel_title: 'Test ' + x, video_title: 'Test video ' + x}));
+		$container.append($videoItem);
+	}
+
+	$domVideosWrap.insertBefore($('.top-shelf'));
+
+	var totalWidth = $container.children().eq(0).outerWidth(true) * 10;
+	$container.width(totalWidth);
+
+	// console.log("Ready");
+	// var res = Mustache.render(Templates['videoItem'], {channel_title: 'Test', video_title: 'Test video'});
+	// console.log(res);
+});
---
+++ src/static/views/videos-wrap.html.js
@@ -0,0 +1,10 @@
+module.exports = '\
+<section class="bbtv-videos-wrap"> \
+	<h2 class="bbtv-videos-wrap__title">Here is some delicous videos from {{restaurant_name}}</h2> \
+	<div class="bbtv-videos-wrap__items-wrap"> \
+		<div class="bbtv-videos-wrap__items-container"> \
+		</div> \
+	</div> \
+	<button class="bbtv-videos-wrap__open-close-button bbtv-videos-wrap__open-close-button--opened">hide videos</button> \
+</section> \
+';
